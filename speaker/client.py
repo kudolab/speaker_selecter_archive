b3b06490905f6a5002f76e1fdb96f7e99a444625
neqonagomiya <neqofan@gmail.com>
2021-06-02 12:43:36 +0900

    change app.py, add speaker set process

--- speaker/client.py
+++ speaker/client.py
@@ -2,12 +2,14 @@ import requests
 import json
 
 url = http://172.24.176.51/speaker
-speaker_data = {"speaker_num":1}
+speaker_data = {
+	"speaker_num":1
+}
 
 headers = {"content-type":"application/json"}
 speaker_json = json.dumps(speaker_data).encode("utf-8")
 
 request = requests.put(url,speaker_json,headers=headers)
-responce_data = requests.json()
+responce_data = request.json()
 
 
